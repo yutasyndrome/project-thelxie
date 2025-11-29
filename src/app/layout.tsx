63e752549f55774e63bdd4678689cad1ee578668
yutasyndrome <210336651+yutasyndrome@users.noreply.github.com>
2025-11-29 16:17:41 +0900

    feat: apply fonts

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -4,7 +4,22 @@ import Header from '@/components/feature/header';
 import ThemeProvider from '@/components/feature/theme-provider';
 import { SidebarProvider } from '@/components/ui/sidebar';
 import type { Metadata } from 'next';
+import { M_PLUS_1p, Poppins } from 'next/font/google';
 import './globals.css';
+
+const poppins = Poppins({
+  subsets: ['latin'],
+  weight: ['300', '400', '600', '700'],
+  variable: '--font-en',
+  display: 'swap',
+});
+
+const mplus = M_PLUS_1p({
+  subsets: ['latin'],
+  weight: ['300', '400', '500', '700'],
+  variable: '--font-ja',
+  display: 'swap',
+});
 
 export const metadata: Metadata = {
   title: {
@@ -21,7 +36,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="ja" suppressHydrationWarning>
+    <html lang="ja" className={`${poppins.variable} ${mplus.variable}`}suppressHydrationWarning>
       <body>
         <ThemeProvider
           attribute="class"
