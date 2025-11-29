5a0098f762077b09327d353814569962be2b1b43
yutasyndrome <210336651+yutasyndrome@users.noreply.github.com>
2025-11-29 16:53:37 +0900

    feat: add not-found page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,25 @@
+import Container from '@/components/feature/container';
+import { Button } from '@/components/ui/button';
+import Link from 'next/link';
+
+export default function NotFound() {
+  return (
+    <main className="flex-1">
+      <Container>
+        <div className="mt-10 justify-center w-full flex flex-col items-center gap-8 text-center" >
+        <h1 className="text-4xl font-bold">
+          404 - Page Not Found
+        </h1>
+        <p >
+          申し訳ございませんが、お探しのページは見つかりませんでした。
+          <br />
+          URL が変更されたか、ページが削除された可能性がございます。
+        </p>
+        <Link href="/">
+          <Button className="mt-6 bg-sky-400 text-white hover:bg-sky-500">Home へ戻る</Button>
+        </Link>
+        </div>
+      </Container>
+    </main>
+  );
+}
